d26ef2693251cbf0b8110667ea5daf1a64960e19
unknown <Monga@1WP1186-LAP-01.business.rutgers.edu>
2020-01-02 12:04:13 -0500

    didnt do anything

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 window.onload = function() {
   document.getElementById("createTaskButton").onclick = addTask;
+  document.getElementById("markPriorityButton").onclick = markPriority;
+  document.getElementById("deleteTaskButton").onclick = markPriority;
 }
 
 
@@ -11,16 +13,33 @@ const addTask = function() {
   var tasks = []
   tasks.push(newTask);
   if (newTask !== '') {
-    console.log("hello")
+
     var item = document.createElement("li")
     item.innerHTML = newTask;
     document.getElementById("tasklist").appendChild(item);
     var endBreak = document.createElement("BR");
     document.getElementById("tasklist").appendChild(endBreak)
+    var newbutton = document.createElement("");
+    document.getElementById("tasklist").appendChild(endBreak)
   }
   document.getElementById("newTask").value = "";
 }
 
+const markPriority = function() {
 
+}
+
+const deleteTask = () {
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+    }
+  }
+}
+
+// matt says to create span and then prepend elements?????
 
 /// make priority, strikethrough, delete. edit the array when doing so
