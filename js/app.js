b8ae837f1e2b151939b17f66bb6e3727cc46ac73
Nirayka Monga <NMonga@ucvts.tec.nj.us>
2020-01-02 10:49:14 -0500

    finished add task function i think

--- js/app.js
+++ js/app.js
@@ -1,29 +1,26 @@
 window.onload = function() {
-    document.getElementById("mark-priority").onclick = markPriority;
-    document.getElementbyID("add-task").onclick = addTask
-
-    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
-    //
-    // document.getElementById("some-id")   <-- you need to give each button a unique ID
-    //                                          and access it in this manner
-    //
-    // onclick is an event that is fired when you click something (in our case, a button).
-    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
-    // you should set onclick equal to your function` names (i.e., sayHello).
-    //
-    // there are six event listeners being added for the staff solutions. you'll have an
-    // equivalent set of six event listeners for your solutions. the first one is done for you.
-
-    /// give the button this id!!
-
+  document.getElementById("createTaskButton").onclick = addTask;
 }
+
 
 const addTask = function() {
-  var newTask = document.getElementById("newTask").value
-  alert(newTask);
+  var newTask = document.getElementById("newTask").value;
+  if (newTask === '') {
+    alert("Task cannot be blank.");
+  }
+  var tasks = []
+  tasks.push(newTask);
+  if (newTask !== '') {
+    console.log("hello")
+    var item = document.createElement("li")
+    item.innerHTML = newTask;
+    document.getElementById("tasklist").appendChild(item);
+    var endBreak = document.createElement("BR");
+    document.getElementById("tasklist").appendChild(endBreak)
+  }
+  document.getElementById("newTask").value = "";
 }
 
-const markPriority = function() {
-  let imgReplace = document.getElementsByID("notStarred")[0];
-      imgReplace.src = "starred";
-};
+
+
+/// make priority, strikethrough, delete. edit the array when doing so
