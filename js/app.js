bcdb03628e766692033805e6ca89109d2149cfc7
unknown <Monga@1WP1186-LAP-01.business.rutgers.edu>
2020-01-07 17:49:54 -0500

    something

--- js/app.js
+++ js/app.js
@@ -10,14 +10,59 @@ let deleteButtons = [];
 let theTrash = [];
 let taskNumber = 0
 
+let elementSet = [];
+
+// html row is listSection.
+
 addTask = function() {
-  console.log("hi")
   var newTask = document.getElementById("newTask").value;
   console.log(newTask)
   if (newTask === '') {
     alert("Task cannot be blank.");
   }
   else {
+    let taskObject = {
+      listSection: null,
+      finished: false,
+      task: newTask,
+      priority: false,
+      deleted: null
+    }
+    elementSet.push(taskObject);
+
+    var theParentElement = document.getElementsByClassName("listThing")
+
+    let x = elementSet.indexOf(taskObject);
+
+    elementSet[x].listSection = document.createElement("li");
+    elementSet[x].listSection.class = "listThing";
+    document.getElementById("listContainer").append(elementSet[x].listSection);
+
+    elementSet[x].checkBoxButton = document.createElement("INPUT")
+    elementSet[x].checkBoxButton.type = "checkbox"
+    elementSet[x].checkBoxButton.class = "finished"
+    elementSet[x].listSection.append(elementSet[x].checkBoxButton);
+
+    elementSet[x].taskText = document.createTextNode(newTask)
+    elementSet[x].taskText.class = "task"
+    elementSet[x].listSection.appendChild(elementSet[x].taskText);
+
+    elementSet[x].priorityButton = document.createElement("BUTTON");
+    elementSet[x].priorityButton.class = "priority"
+    elementSet[x].priorityButton.innerHTML = "!";
+    elementSet[x].listSection.append(elementSet[x].priorityButton);
+
+    elementSet[x].deleteButton = document.createElement("BUTTON");
+    elementSet[x].deleteButton.setAttribute.class = "deleted"
+    elementSet[x].deleteButton.innerHTML = "X";
+    elementSet[x].listSection.append(elementSet[x].deleteButton);
+  }
+  document.getElementById("newTask").value = "";
+  taskNumber++
+}
+
+
+stupid = function() {
     var unchecked = document.createElement("IMG")
     unchecked.innerHTML = "<img src='images/uncheckedcircle.png' width='14' height='14'>";
     theCheckboxes.push(unchecked)
@@ -40,13 +85,7 @@ addTask = function() {
     trashButton.appendChild(trashcan)
 
     console.log("what")
-
-
   }
-  showTask()
-  document.getElementById("newTask").value = "";
-  taskNumber++
-}
 
 
 const showTask = function() {
