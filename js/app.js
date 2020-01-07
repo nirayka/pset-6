50a74dbbc129744b13a27f4472b9695be8d8e22e
unknown <Monga@1WP1186-LAP-01.business.rutgers.edu>
2020-01-07 13:21:30 -0500

    well

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@ window.onload = function() {
   document.getElementById("createTaskButton").onclick = addTask;
 }
 
-
-
 let theCheckboxes = [];
 let theTasks = [];
+let priorityButtons = [];
 let theStars = [];
+let deleteButtons = [];
 let theTrash = [];
+let taskNumber = 0
 
 addTask = function() {
   console.log("hi")
@@ -23,16 +24,38 @@ addTask = function() {
 
     theTasks.push(newTask)
 
+    var priorityButton = document.createElement("BUTTON");
+    priorityButtons.push(priorityButton)
     var emptyStar = document.createElement("IMG")
     emptyStar.innerHTML = "<img src='images/emptystar.png' width='14' height='14'>";
+    theStars.push(emptyStar)
+    priorityButton.appendChild(emptyStar)
+
+    var trashButton = document.createElement("BUTTON");
+    trashButton.onclick = deleteTask;
+    deleteButtons.push(trashButton)
+    var trashcan = document.createElement("IMG")
+    trashcan.innerHTML = "<img src='images/trashcan.png' width='14' height='14'>";
+    theTrash.push(trashcan)
+    trashButton.appendChild(trashcan)
+
+    console.log("what")
 
 
   }
   showTask()
   document.getElementById("newTask").value = "";
+  taskNumber++
 }
 
 
 const showTask = function() {
   console.log("bae")
+  var listContainer = document.getElementById("listContainer")
+  listContainer.appendChild(newTask, priorityButton, trashButton)
+}
+
+
+const deleteTask = function() {
+  console.log("delete")
 }
